72cdd0a7ac232710a05daff64106a2226f007b6f
thegalgos <86036870+thegalgos@users.noreply.github.com>
2022-01-22 18:20:15 -0300

    Updates

---
+++ static/js/4.77ec540d.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpreact-tailwind-typescript"]=this["webpackJsonpreact-tailwind-typescript"]||[]).push([[4],{152:function(t,n){function e(t){var n=new Error("Cannot find module '"+t+"'");throw n.code="MODULE_NOT_FOUND",n}e.keys=function(){return[]},e.resolve=e,t.exports=e,e.id=152}}]);
+//# sourceMappingURL=4.77ec540d.chunk.js.map
